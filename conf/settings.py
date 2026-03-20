d9de26d09efb9e67b301673e1bd61bf32959cfe2
Abdumannon Rabbimqulov <abdumannonrab@icloud.com>
2026-03-20 09:04:18 +0500

    drf

--- conf/settings.py
+++ conf/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from .models import CodeVerify
+from datetime import datetime
+from rest_framework.exceptions import ValidationError
+import random
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+def send_email(user):
+    active_codes=CodeVerify.objects.filter(user=user,expiration_time__gte=datetime.now())
+    if active_codes.exists():
+        raise ValidationError({'message':'sizda active code bor'})
+    code =random.randint(1000,9999)
+    CodeVerify.objects.create(
+        code=code,
+        user=user,
+    )
+
+    try:
+        send_mail(
+            "Tasdiqlash kodi",
+            f" Sizning tasdiqlash kodingiz {code}",
+            settings.EMAIL_HOST_USER,
+            [user.email],
+            fail_silently = False,
+        )
+        return True
+    except Exception as e:
+        raise ValidationError({'message':f" Email yuborishda xatolik: {e}"})
