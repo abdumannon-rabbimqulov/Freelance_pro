0aa4e7032490ea529bfb408ee47c314be4316dbe
Abdumannon Rabbimqulov <abdumannonrab@icloud.com>
2026-03-25 10:50:55 +0500

    dd

--- conf/settings.py
+++ conf/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'drf_spectacular',
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "users",
@@ -55,9 +56,17 @@ REST_FRAMEWORK={
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Freelance Pro API',
+    'DESCRIPTION': 'Freelance platformasi uchun API hujjatlari',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
